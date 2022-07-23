6adb6099bb2d1541f4fd01bef9c0f24b70831108
seojeongkuk <wjdrnr6662@gmail.com>
2022-07-23 13:52:18 +0900

    Member 저장하는 코드 추가

--- test/src/main/java/hellojpa/JpaMain.java
+++ test/src/main/java/hellojpa/JpaMain.java
@@ -2,12 +2,23 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("꾹이..");
+
+        entityManager.persist(member);
+        tx.commit();
+
         entityManager.close();
         emf.close();
     }
