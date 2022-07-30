7548abb6a4024847b7a288c366bf43f3bdb38587
seojeongkuk <wjdrnr6662@gmail.com>
2022-07-30 15:45:22 +0900

    entity 데코레이터 및 환경변수 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
