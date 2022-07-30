94d83afc17887839ca9c879e37b6771295db0a13
seojeongkuk <wjdrnr6662@gmail.com>
2022-07-30 15:22:14 +0900

    JpaMain 추가

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        tx.commit();
+        entityManager.close();
+        emf.close();
+    }
+}
