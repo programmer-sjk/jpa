7ee295e68468da05f664cefecf0db2160f56dacc
seojeongkuk <wjdrnr6662@gmail.com>
2022-08-04 22:06:46 +0900

    1대1 연관관계

--- test/src/main/java/hellojpa/JpaMain.java
+++ test/src/main/java/hellojpa/JpaMain.java
@@ -16,20 +16,20 @@ public class JpaMain {
         Team team = new Team();
         team.setName("teamA");
         entityManager.persist(team);
-
-        Member member = new Member();
-        member.setUsername("member1");
-        member.changeTeam(team);
-        entityManager.persist(member);
-
-        Team findTeam = entityManager.find(Team.class, team.getId());
-        List<Member> members = findTeam.getMembers();
-
-        System.out.println("=========");
-        for(Member m : members) {
-            System.out.println("m = " + m.getUsername());
-        }
-        System.out.println("=========");
+//
+//        Member member = new Member();
+//        member.setUsername("member1");
+//        member.changeTeam(team);
+//        entityManager.persist(member);
+//
+//        Team findTeam = entityManager.find(Team.class, team.getId());
+//        List<Member> members = findTeam.getMembers();
+//
+//        System.out.println("=========");
+//        for(Member m : members) {
+//            System.out.println("m = " + m.getUsername());
+//        }
+//        System.out.println("=========");
 
         tx.commit();
         entityManager.close();
