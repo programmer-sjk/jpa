2f1ff6aa9318b2df70b2798d836f2527096a57ce
seojeongkuk <wjdrnr6662@gmail.com>
2022-08-13 16:36:29 +0900

    상속관게 및 mappedsuperclass 실습

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop;
 
 
+import jpabook.jpashop.domain.Book;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -12,6 +14,11 @@ public class JpaMain {
         EntityManager entityManager = emf.createEntityManager();
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
+
+        Book book = new Book();
+        book.setName("JPA");
+        book.setAuthor("서정국");
+        entityManager.persist(book);
 
         tx.commit();
         entityManager.close();
