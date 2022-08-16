c4fd7b69d6f5fc20e26a97dbed6f0ff4a629be0b
seojeongkuk <wjdrnr6662@gmail.com>
2022-08-16 15:39:22 +0900

    jpql 실습에 필요한  엔티티 정의

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,10 +1,9 @@
-package hellojpa;
+package jpql;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -12,11 +11,6 @@ public class JpaMain {
         EntityManager entityManager = emf.createEntityManager();
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
-
-        List<Member> members = entityManager.createQuery(
-                "select m from Member m where m.username like '%kim%'",
-                Member.class
-        ).getResultList();
 
         tx.commit();
         entityManager.close();
