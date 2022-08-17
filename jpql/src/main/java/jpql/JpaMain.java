2ef71c3829dceb7f9b4bfb5a6ac07f6cc6901887
seojeongkuk <wjdrnr6662@gmail.com>
2022-08-17 16:29:11 +0900

    dto를 이용한 jpql 조회

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,12 @@
 package jpql;
 
+import jpql.dto.MemberDto;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -11,6 +14,19 @@ public class JpaMain {
         EntityManager entityManager = emf.createEntityManager();
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
+
+        Member member = new Member();
+        member.setUsername("꾸기");
+        member.setAge(33);
+        entityManager.persist(member);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        List<MemberDto> result = entityManager.createQuery("select new jpql.dto.MemberDto(m.username, m.age) from Member m", MemberDto.class).getResultList();
+        MemberDto findMember = result.get(0);
+
+        System.out.println("memberDto = " + findMember.getUsername());
 
         tx.commit();
         entityManager.close();
