8245e53928e4d4a4c0dfd41b1130778fd4c7cb4a
sjk5766 <wjdrnr6662@gmail.com>
2023-01-04 09:35:52 +0900

    chapter 7: MappedSuperClass를 이용한 컬럼 정보 상속

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -16,20 +16,6 @@ public class JpaMain {
 
         try {
             tx.begin();
-            Product product = new Product();
-            product.setId("A");
-            product.setName("name");
-            em.persist(product);
-
-            Member member = new Member();
-            member.setId(1L);
-            member.setUsername("member");
-            em.persist(member);
-
-            Order order = new Order();
-            order.setMember(member);
-            order.setProduct(product);
-            em.persist(order);
 
             tx.commit();
         } catch (Exception e) {
@@ -38,53 +24,6 @@ public class JpaMain {
         } finally {
             em.close();
         }
-
         emf.close();
-    }
-
-    private static void testSave(EntityManager em) {
-        Team team1 = new Team("team1", "팀1");
-        em.persist(team1);
-
-        Member member1 = new Member(1L, "회원1", 33);
-        member1.setTeam(team1);
-        em.persist(member1);
-
-        Member member2 = new Member(2L, "회원2", 33);
-        member2.setTeam(team1);
-        em.persist(member2);
-    }
-
-    private static void query(EntityManager em) {
-        String jpql = "select m from Member m join m.team t where t.name = :teamName";
-        List<Member> results = em.createQuery(jpql, Member.class)
-                .setParameter("teamName", "팀1")
-                .getResultList();
-
-        for (Member member : results) {
-            System.out.println(member.getUsername());
-        }
-    }
-
-    private static void update(EntityManager em) {
-        Team team2 = new Team("team2", "팀2");
-        em.persist(team2);
-
-        Member member = em.find(Member.class, 1L);
-        member.setTeam(team2);
-    }
-
-    private static void delete(EntityManager em) {
-        Member member = em.find(Member.class, 1L);
-        member.setTeam(null);
-    }
-
-    private static void biDirection(EntityManager em) {
-        Team team = em.find(Team.class, "team1");
-        List<Member> members = team.getMembers();
-
-        for (Member member : members) {
-            System.out.println(member.getUsername());
-        }
     }
 }
