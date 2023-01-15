02bc414b1504ca95e5d0ae2e0725ffab173a4565
sjk5766 <wjdrnr6662@gmail.com>
2023-01-15 21:52:42 +0900

    3장. 1차 캐시 find 예제

--- jpa-example/src/main/java/JpaMain.java
+++ jpa-example/src/main/java/JpaMain.java
@@ -1,3 +1,5 @@
+import domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -6,12 +8,21 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("example");
-        EntityManager entityManager = emf.createEntityManager();
-        EntityTransaction tx = entityManager.getTransaction();
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+        Member member = new Member();
+        member.setId(1L);
+        em.persist(member);
         tx.commit();
-        entityManager.close();
+
+        tx.begin();
+        em.clear();
+        Member findMember = em.find(Member.class, 1L);
+        tx.commit();
+
+        em.close();
         emf.close();
     }
 }
--- jpa-example/src/main/java/domain/Member.java
+++ jpa-example/src/main/java/domain/Member.java
@@ -4,6 +4,10 @@ import javax.persistence.*;
 
 @Entity
 public class Member {
-    @Id @GeneratedValue
+    @Id
     private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
