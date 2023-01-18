fddee49fa4b1ed364edd40aa4ed8a7eb5af5c51a
sjk5766 <wjdrnr6662@gmail.com>
2023-01-18 11:11:47 +0900

    8장. 지연로딩

--- jpa-example/src/main/java/JpaMain.java
+++ jpa-example/src/main/java/JpaMain.java
@@ -1,6 +1,7 @@
 import domain.Member;
 import domain.Orders;
 import domain.Product;
+import domain.Team;
 
 import javax.persistence.*;
 
@@ -11,14 +12,20 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
+        Team team = new Team("team1");
+        em.persist(team);
+
         Member member = new Member();
         member.setId("member1");
         member.setName("서정국");
+        member.setTeam(team);
         em.persist(member);
 
         em.flush();
         em.clear();
         Member findMember = em.find(Member.class, "member1");
+        System.out.println("====");
+        System.out.println(em.getEntityManagerFactory().getPersistenceUnitUtil().isLoaded(findMember.getTeam()));
         tx.commit();
 
         em.close();
